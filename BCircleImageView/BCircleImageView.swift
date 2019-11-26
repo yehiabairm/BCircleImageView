55945ea26f024c84793e995e0cb222bd5077d0ca
yehia bairm <yehiabairm@gmail.com>
2019-11-26 13:48:56 +0200

    Added test cases

---
+++ BCircleImageView/BCircleImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImageView.swift
+//  BCircleImageView
+//
+//  Created by Yehia Beram on 11/26/19.
+//  Copyright © 2019 Yehia Beram. All rights reserved.
+//
+
+import Foundation
+
+public final class BCircleImageView {
+    let name = "BCircleImageView"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
